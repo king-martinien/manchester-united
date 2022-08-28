1edc9232761434e1e3e7e48e8425f671c7e24472
king-martinien <martinienfokoue@yahoo.com>
2022-08-28 04:44:57 +0100

    section videos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,14 @@
 let burger = document.querySelector('.burger')
+let header = document.querySelector('.header')
+
 burger.addEventListener('click', () => {
     burger.classList.toggle('toggled')
+})
+
+document.addEventListener('scroll', (e) => {
+    if (scrollY > header.clientHeight) {
+        header.classList.add('scrolled')
+    } else {
+        header.classList.remove('scrolled')
+    }
 })
